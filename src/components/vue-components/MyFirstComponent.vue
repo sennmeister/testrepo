72c469a61601a9bab32fdaa58dc3b8cbb7ab83f7
Werner Frischknecht <werfris@gmail.com>
2023-12-23 17:30:36 +0100

    tests

---
+++ src/components/vue-components/MyFirstComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>Hallo Senn</h2>
+    </div>
+
+    <div class="fotos">
+        <p>foto</p>
+    </div>
+
+</template>
+
+
+<script>
+
+export default {
+  name: "MyFirstComponent"
+  }
+
+</script>
+
+
+
+<style lang="sass" scoped>
+
+div
+    color: red
+
+    img
+        width: 500px
+.fotos
+    background-color: green
+
+</style>
